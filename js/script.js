49fd11def7a8210ef2c6c852e04ab1875cd83717
danielmartinezkrams <danielmartinezkrams@students.berkeley.net>
2017-05-18 10:26:59 -0700

    Playing Around with Widget 1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@
  */
 function widget(){
     var mp = document.createElement('script'),h=document.getElementsByTagName('head')[0];
-    mp.type='text/javascript';mp.async=true;
+    mp.type='text/javascript';
+    mp.async=true;
     mp.src='http://www.maxpreps.com/includes/js/widget/widget.compressed.js';
     h.appendChild(mp);
 }
