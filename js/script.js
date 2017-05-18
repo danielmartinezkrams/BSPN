a573baa9fd14e946d0b1e0fc12609774c1fab2d2
danielmartinezkrams <danielmartinezkrams@students.berkeley.net>
2017-05-18 10:33:27 -0700

    End of Day 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /**
  * Created by h205p2 on 5/18/17.
  */
+/*
 function widget(){
     var mp = document.createElement('script'),h=document.getElementsByTagName('head')[0];
     mp.type='text/javascript';
@@ -8,3 +9,4 @@ function widget(){
     mp.src='http://www.maxpreps.com/includes/js/widget/widget.compressed.js';
     h.appendChild(mp);
 }
+*/
