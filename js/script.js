7adbb22a8b6ec518528b753e2729d2dcafd204a1
danielmartinezkrams <danielmartinezkrams@students.berkeley.net>
2017-05-18 10:22:03 -0700

    Header Made Shit Added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 /**
  * Created by h205p2 on 5/18/17.
  */
+function widget(){
+    var mp = document.createElement('script'),h=document.getElementsByTagName('head')[0];
+    mp.type='text/javascript';mp.async=true;
+    mp.src='http://www.maxpreps.com/includes/js/widget/widget.compressed.js';
+    h.appendChild(mp);
+}
