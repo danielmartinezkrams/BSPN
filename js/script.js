a556c9e49f14792a32d6d2b4cc88941b30e28a6b
h205p2 <mattalbinson@berkeley.net>
2017-05-25 10:06:12 -0700

    back button on sportsB that takes you back to sportsA

--- js/script.js
+++ js/script.js
@@ -1,22 +1,14 @@
 /**
  * Created by h205p2 on 5/18/17.
  */
-/*
-function widget(){
-    var mp = document.createElement('script'),h=document.getElementsByTagName('head')[0];
-    mp.type='text/javascript';
-    mp.async=true;
-    mp.src='http://www.maxpreps.com/includes/js/widget/widget.compressed.js';
-    h.appendChild(mp);
-}
-*/
+
 
 
 //berkeleyspn@gmail.com
 //GoJackets!
 
 
-//<a class="twitter-timeline" href="https://twitter.com/BHSjacketSports">Tweets by BHSjacketSports</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
 
 
 
