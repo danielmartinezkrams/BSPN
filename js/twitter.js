fb701f670b8eb144ece6a40d15b884bd45746e01
danielmartinezkrams <danielmartinezkrams@students.berkeley.net>
2017-06-01 10:34:40 -0700

    twitter...

--- js/twitter.js
+++ js/twitter.js
@@ -1,28 +1,21 @@
 $(document).ready(function() {
 
-        var word = "BHSjacketSports";
+
         var configProfile = {
-            "profile": {"screenName": word},
+            "profile": {"screenName": "BHSjacketSports"},
             "domId": 'example1',
             /*
              this is the number of tweets that it'll get
              */
             "maxTweets": 10,
-
-
-
             "enableLinks": true,
             "showUser": true,
             "showTime": true,
-            "showImages": false,
+            "showImages": true,
             "lang": 'en'
         };
         twitterFetcher.fetch(configProfile);
         console.log(allTweetText);
-    /*var $twitterList = $("#twitterList")
-    for(var i = 0; i  < allTweetText.length; i++){
-        $($twitterList).append("<li>" + allTweetText[i] + "</li>")
-    }
-    */
+
 
 });
