dfc5209bde4c2d2e56537d80b88ebacb6d25c971
h205p2 <ketanprunet@students.berkeley.net>
2017-06-02 10:30:54 -0700

    signup shit

--- js/data.js
+++ js/data.js
@@ -111,7 +111,7 @@ $(document).ready(function(){
             authorized = true;
         }
         if (firstName == '' || lastName == '' || email == '' || password == '' || passwordConfirm == '') {
-            alert("Please fill all fields...!!!!!!");
+            var signUpPassword =
         }
         else if (!(password).match(passwordConfirm)) {
             alert("Your passwords don't match. Try again?");
