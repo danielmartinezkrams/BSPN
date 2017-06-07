83cbed9548b498f0c99d99c33513519d3ed71d70
danielmartinezkrams <danielmartinezkrams@students.berkeley.net>
2017-06-07 10:12:19 -0700

    look good, feel good, play good

--- js/twitter.js
+++ js/twitter.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
-    var configBHSJacketSports = {
-            "profile": {"screenName": "BHSjacketSports"},
-            "domId": 'example1',
-            "maxTweets": 20,
-            "enableLinks": true,
-            "showUser": true,
-            "showTime": true,
-            "showImages": true,
-            "lang": 'en'
-        };
+
     var configBSPN = {
         "profile": {"screenName": "BerkeleySPN"},
-        "domId": 'example1',
+        "domId": 'Twitter1',
+        "maxTweets": 20,
+        "enableLinks": true,
+        "showUser": true,
+        "showTime": true,
+        "showImages": true,
+        "lang": 'en'
+    };
+    twitterFetcher.fetch(configBSPN);
+    console.log(allTweetText);
+
+
+    var configBHSJacketSports = {
+        "profile": {"screenName": "BHSjacketSports"},
+        "domId": 'Twitter2',
         "maxTweets": 20,
         "enableLinks": true,
         "showUser": true,
@@ -20,6 +25,8 @@ $(document).ready(function() {
         "lang": 'en'
     };
     twitterFetcher.fetch(configBHSJacketSports);
-    twitterFetcher.fetch(configBSPN);
-    //console.log(allTweetText);
+
+
+    console.log(allTweetText);
+
 });
